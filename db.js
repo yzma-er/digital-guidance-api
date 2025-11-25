5fcfd9636fd4b662f856e828e11cf466f9617352
yzma-er <bumolyadysmael@gmail.com>
2025-11-25 15:50:43 +0800

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,   
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306
 });
